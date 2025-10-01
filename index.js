514edd83e26ba203bb86108d62e509ffde8b669b
therealminigirl-ship-it <therealminigirl@gmail.com>
2025-10-01 23:09:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
+
+let password1El = document.getElementById("password1-el")
+let password2El = document.getElementById("password2-el")
+let userInput = document.getElementById("user-input")
+let errorMsg = document.getElementById("error-msg")
+let inputCheck = true
+
+function generate() {
+        let passwordFirst = ""
+        let passwordSecond = ""
+        errorMsg.textContent = ""
+        let passwordLength = parseInt(userInput.value)
+    if (isNaN(passwordLength) || passwordLength <= 0 || passwordLength>16) {
+        inputCheck = false
+        passwordLength = 15 
+        errorMsg.textContent = "Invalid input, 15 character password generated."
+        }
+    for (let i=0; i<passwordLength;i++) {
+        passwordFirst += characters[Math.floor(Math.random() * characters.length)]
+        passwordSecond += characters[Math.floor(Math.random() * characters.length)]
+        }
+        password1El.textContent = passwordFirst
+        password2El.textContent = passwordSecond
+}
+            
+
+// function copy(id) {
+//    navigator.clipboard.writeText(id.textContent)
+//    errorMsg.textContent = "Copied to clipboard"
+//    }
+
